d872fdf54ac3bdb6e16eb68c9ab3e14615099bd9
Crowdbotics <team@crowdbotics.com>
2023-01-29 06:07:24 +0000

    Initial commit.

---
+++ zamcops_38556/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zamcops_38556 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zamcops_38556.settings')
+
+application = get_wsgi_application()
